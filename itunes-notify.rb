8e4496ed138b28cffb41d89cb8b754a104e5ba2d
Sorin Ionescu <sorin.ionescu@gmail.com>
2013-01-09 18:00:06 -0500

    Set the hompage first

--- itunes-notify.rb
+++ itunes-notify.rb
@@ -1,8 +1,8 @@
 require 'formula'
 
 class ItunesNotify < Formula
-  head 'http://cmus.sourceforge.net/wiki/lib/exe/fetch.php?media=itunes-notify.c.zip'
   homepage 'http://cmus.sourceforge.net/wiki/doku.php?id=status_display_programs'
+  head 'http://cmus.sourceforge.net/wiki/lib/exe/fetch.php?media=itunes-notify.c.zip'
 
   def install
     system 'cc itunes-notify.c -framework CoreFoundation -o itunes-notify'
