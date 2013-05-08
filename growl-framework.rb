08779f50555700494895f2e4c4b91864d2a810ab
Sorin Ionescu <sorin.ionescu@gmail.com>
2013-05-08 16:01:58 -0400

    Add growl-framework

---
+++ growl-framework.rb
@@ -0,0 +1,11 @@
+require 'formula'
+
+class GrowlFramework < Formula
+  homepage 'http://growl.info'
+  url 'http://growl.cachefly.net/Growl-2.0.1-SDK.zip'
+  sha1 'c69b3026661aec6a8455ba9b6e90452e0c1e89aa'
+
+  def install
+    (prefix + 'Frameworks').install 'Framework/Growl.framework'
+  end
+end
