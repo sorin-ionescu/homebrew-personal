8764be9d11be899a93a5a15a3b2c3a0cdadeabf3
Sorin Ionescu <sorin.ionescu@gmail.com>
2013-11-20 17:31:27 -0500

    Do not change dvtm modifier

--- dvtm.rb
+++ dvtm.rb
@@ -6,10 +6,6 @@ class Dvtm < Formula
   sha1 '74b9d1f5172fddd6839d932e483d36c6d0ef4b04'
   head 'git://repo.or.cz/dvtm.git'
 
-  def patches
-    # Change the modifier from Ctrl + G to Ctrl + \.
-    DATA
-  end
 
   def install
     inreplace 'config.mk', 'LIBS = -lc -lutil -lncursesw', 'LIBS = -lc -lutil -lncurses'
@@ -17,17 +13,3 @@ class Dvtm < Formula
     system "make", "PREFIX=#{prefix}", "install"
   end
 end
-__END__
-diff --git 1/dvtm-0.7.a/config.h 2/dvtm-0.7.b/config.h
-index 2b00776..cb00de6 100644
---- 1/config.h
-+++ 2/config.h
-@@ -53,7 +53,7 @@ Layout layouts[] = {
- 	{ "[ ]", fullscreen },
- };
-
--#define MOD CTRL('g')
-+#define MOD CTRL('\\')
-
- /* you can at most specifiy MAX_ARGS (2) number of arguments */
- Key keys[] = {
