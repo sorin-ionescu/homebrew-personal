da336d6c9b44849206b9a871beadd728575453f5
Sorin Ionescu <sorin.ionescu@gmail.com>
2013-11-20 14:34:53 -0500

    Sync ncurses with upstream

--- ncurses.rb
+++ ncurses.rb
@@ -12,7 +12,10 @@ class Ncurses < Formula
 
   # Fix building C++ bindings with clang
   def patches
-    { :p0 => "https://trac.macports.org/export/103963/trunk/dports/devel/ncurses/files/constructor_types.diff" }
+    { :p0 => "https://trac.macports.org/export/103963/trunk/dports/devel/ncurses/files/constructor_types.diff",
+      # Stop using obsolete -no-cpp-precomp flag, which breaks FSF GCC
+      # Reported upstream by email
+      :p1 => DATA }
   end
 
   def install
@@ -56,3 +59,30 @@ class Ncurses < Formula
     end
   end
 end
+
+__END__
+diff --git a/Ada95/configure b/Ada95/configure
+index 4db6f1f..e82bb4b 100755
+--- a/Ada95/configure
++++ b/Ada95/configure
+@@ -7460,7 +7460,6 @@ CF_EOF
+ 		chmod +x mk_shared_lib.sh
+ 		;;
+ 	darwin*) #(vi
+-		EXTRA_CFLAGS="-no-cpp-precomp"
+ 		CC_SHARED_OPTS="-dynamic"
+ 		MK_SHARED_LIB='${CC} ${CFLAGS} -dynamiclib -install_name ${libdir}/`basename $@` -compatibility_version ${ABI_VERSION} -current_version ${ABI_VERSION} -o $@'
+ 		test "$cf_cv_shlib_version" = auto && cf_cv_shlib_version=abi
+diff --git a/configure b/configure
+index 639b790..25d69b3 100755
+--- a/configure
++++ b/configure
+@@ -5584,7 +5584,6 @@ CF_EOF
+ 		chmod +x mk_shared_lib.sh
+ 		;;
+ 	darwin*) #(vi
+-		EXTRA_CFLAGS="-no-cpp-precomp"
+ 		CC_SHARED_OPTS="-dynamic"
+ 		MK_SHARED_LIB='${CC} ${CFLAGS} -dynamiclib -install_name ${libdir}/`basename $@` -compatibility_version ${ABI_VERSION} -current_version ${ABI_VERSION} -o $@'
+ 		test "$cf_cv_shlib_version" = auto && cf_cv_shlib_version=abi
+
