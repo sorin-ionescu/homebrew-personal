2e0a79adceafd314ed2b775a15fbafeb0e18b130
Sorin Ionescu <sorin.ionescu@gmail.com>
2013-11-20 17:40:12 -0500

    Build dvtm against ncursesw

--- dvtm.rb
+++ dvtm.rb
@@ -6,9 +6,9 @@ class Dvtm < Formula
   sha1 '74b9d1f5172fddd6839d932e483d36c6d0ef4b04'
   head 'git://repo.or.cz/dvtm.git'
 
+  depends_on 'ncurses'
 
   def install
-    inreplace 'config.mk', 'LIBS = -lc -lutil -lncursesw', 'LIBS = -lc -lutil -lncurses'
     inreplace 'Makefile', 'strip -s', 'strip'
     system "make", "PREFIX=#{prefix}", "install"
   end
