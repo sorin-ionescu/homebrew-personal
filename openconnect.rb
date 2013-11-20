f8d4da6729e8df5fe2e8da5f428628fc5ecea45f
Sorin Ionescu <sorin.ionescu@gmail.com>
2013-11-20 17:15:41 -0500

    Add openconnect

---
+++ openconnect.rb
@@ -0,0 +1,43 @@
+require 'formula'
+
+class Openconnect < Formula
+  homepage 'http://www.infradead.org/openconnect.html'
+  url 'ftp://ftp.infradead.org/pub/openconnect/openconnect-5.01.tar.gz'
+  sha1 'e0016f56749528b021c74432353c71ffaaf55faa'
+
+  depends_on 'pkg-config' => :build
+  depends_on 'gettext'
+
+  resource 'vpnc-script' do
+    url 'http://git.infradead.org/users/dwmw2/vpnc-scripts.git/blob_plain/d2c5a77f3f0ea6ad80fc59158127d63ede81a6cb:/vpnc-script'
+    sha1 '9915539c34393c1f8d7de9c3fc2c7396476bd998'
+  end
+
+  def patches
+    # Fix Cisco AnyConnect.
+    'http://git.infradead.org/users/dwmw2/openconnect.git/commitdiff_plain/5c4d37a138ed63358315cd23a465f187db7893fc'
+  end
+
+  def install
+    etc.install resource('vpnc-script')
+    chmod 0755, "#{etc}/vpnc-script"
+
+    args = %W[
+      --prefix=#{prefix}
+      --sbindir=#{bin}
+      --localstatedir=#{var}
+      --with-vpnc-script=#{etc}/vpnc-script
+    ]
+
+    system "./configure", *args
+    system "make install"
+  end
+
+  def caveats; <<-EOS.undent
+    OpenConnect requires the use of a TUN/TAP driver.
+
+    You can download one at http://tuntaposx.sourceforge.net/
+    and install it prior to running OpenConnect.
+    EOS
+  end
+end
