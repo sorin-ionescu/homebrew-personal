95baacbd5b8556f669b58784410f2950bf94b097
Sorin Ionescu <sorin.ionescu@gmail.com>
2013-12-26 19:07:21 -0500

    Add missing SHA-1 hash

--- attach.rb
+++ attach.rb
@@ -3,6 +3,7 @@ require 'formula'
 class Attach < Formula
   url 'https://github.com/sorin-ionescu/attach/archive/1.0.7.tar.gz'
   homepage 'https://github.com/sorin-ionescu/attach'
+  sha1 'db5ba251fc840e2ddb09afd28cceaedfcafe6e4f'
   head 'https://github.com/sorin-ionescu/attach.git'
 
   depends_on 'dtach'
