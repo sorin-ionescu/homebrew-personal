d0a92757e86a4dc26db81f4d1e2173b8777eb1dc
Sorin Ionescu <sorin.ionescu@gmail.com>
2013-12-27 17:03:24 -0500

    Update gnu-grep to 2.15

--- gnu-grep.rb
+++ gnu-grep.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class GnuGrep < Formula
   homepage 'http://www.gnu.org/software/grep/'
-  url 'http://ftpmirror.gnu.org/grep/grep-2.14.tar.xz'
-  mirror 'http://ftp.gnu.org/gnu/grep/grep-2.14.tar.xz'
-  sha1 'fb6ea404a0ef915334ca6212c7b517432ffe193e'
+  url 'http://ftpmirror.gnu.org/grep/grep-2.15.tar.xz'
+  mirror 'http://ftp.gnu.org/gnu/grep/grep-2.15.tar.xz'
+  sha1 '1dffd7a82761166cc4d39727944655233c2d95fd'
 
   depends_on 'pcre'
 
