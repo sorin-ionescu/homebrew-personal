a4b661f71efa38015baf3409e3251cc40c6aca22
Sorin Ionescu <sorin.ionescu@gmail.com>
2014-01-28 17:59:00 -0500

    Rename attach to atach

---
+++ atach.rb
@@ -0,0 +1,19 @@
+require 'formula'
+
+class Atach < Formula
+  url 'https://github.com/sorin-ionescu/attach/archive/v1.0.8.tar.gz'
+  homepage 'https://github.com/sorin-ionescu/atach'
+  sha1 '0150d6db71040d3e602cf628d677df843bbcedd3'
+  head 'https://github.com/sorin-ionescu/atach.git'
+
+  depends_on 'dtach'
+
+  def install
+    bin.install 'atach'
+    man1.install 'atach.1'
+  end
+
+  test do
+    system 'attach', '--version'
+  end
+end
