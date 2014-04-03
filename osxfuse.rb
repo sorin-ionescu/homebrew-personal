7b92459e4a730e3c1ca6187487f75e55c61aa735
Sorin Ionescu <sorin.ionescu@gmail.com>
2014-04-03 17:14:42 -0400

    Bump OSXFUSE version to 2.6.4

--- osxfuse.rb
+++ osxfuse.rb
@@ -16,7 +16,7 @@ class Osxfuse < Formula
   homepage 'http://osxfuse.github.io'
   url 'http://example.com', :using => :nounzip
   sha1 '0e973b59f476007fd10f87f347c3956065516fc0'
-  version '2.6.3'
+  version '2.6.4'
 
   depends_on OsxfuseInstalled
   depends_on :macos => :snow_leopard
