c69b1d5ba436b61dbba0bc3e585de5289372281a
Sorin Ionescu <sorin.ionescu@gmail.com>
2014-10-03 17:02:00 -0400

    Use frameworks directory location

--- growl-framework.rb
+++ growl-framework.rb
@@ -6,6 +6,6 @@ class GrowlFramework < Formula
   sha1 'c69b3026661aec6a8455ba9b6e90452e0c1e89aa'
 
   def install
-    (prefix + 'Frameworks').install 'Framework/Growl.framework'
+    frameworks.install 'Framework/Growl.framework'
   end
 end
