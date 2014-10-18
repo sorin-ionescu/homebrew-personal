31a477450bec7751ce73f03238bdd0c1c9c887ca
Sorin Ionescu <sorin.ionescu@gmail.com>
2014-10-18 19:59:34 -0400

    Bump MiniDLNA version to 1.1.4

--- minidlna.rb
+++ minidlna.rb
@@ -1,16 +1,18 @@
 require 'formula'
 
 class Minidlna < Formula
-  url 'https://downloads.sourceforge.net/project/minidlna/minidlna/1.1.2/minidlna-1.1.2.tar.gz'
+  url 'https://downloads.sourceforge.net/project/minidlna/minidlna/1.1.4/minidlna-1.1.4.tar.gz'
   homepage 'http://sourceforge.net/projects/minidlna/'
-  sha1 'ba03d691b9aa95b55ac822905571ab1127492344'
+  sha1 '56f333f8af91105ce5f0861d1f1918ebf5b0a028'
   head 'http://git.code.sf.net/p/minidlna/git', :using => :git
+  revision 1
 
   option 'with-tivo', 'Build with TiVo support'
 
   depends_on 'automake' => :build
   depends_on 'autoconf' => :build
   depends_on 'libtool' => :build
+  depends_on 'gettext' => :build
   depends_on 'exif'
   depends_on 'ffmpeg'
   depends_on 'flac'
@@ -18,48 +20,83 @@ class Minidlna < Formula
   depends_on 'libid3tag'
   depends_on 'libvorbis'
 
+  # Use the Apple icon.
+  # patch do
+  #   url 'https://gist.githubusercontent.com/sorin-ionescu/8118073/raw/e2c62c5a360dc4bccd52c9af36981f84674c8c4f/icon.patch'
+  #   sha1 'ee3a2ba571b2643f08df92727ba442c2e1b8fda0'
+  # end
+
+  # Homebrew downloads the wrong patch using the commented code above.
   def patches
-    [
-      # Fix the undeclared 'IFF_SLAVE' build error.
-      'https://gist.github.com/sorin-ionescu/8118073/raw/eebb97a57c7af9bec8e7763e622edfce62671f0f/getifaddr.c.patch',
-      # Use the Apple icon.
-      'https://gist.github.com/sorin-ionescu/8118073/raw/e2c62c5a360dc4bccd52c9af36981f84674c8c4f/icons.c.patch',
-      # Enable the '-S' switch to disable forking for launchd.
-      'https://gist.github.com/sorin-ionescu/8118073/raw/0a9c88adc99b9571a2a9c553cad29d43e89ac4d8/minidlna.c.patch',
-      # Use FSEvents for filesystem notifications.
-      'https://gist.github.com/sorin-ionescu/8118073/raw/c103c6cb6d6b0eeadbeeebb46f0657b503cc3bc3/fsevents.patch'
-    ]
+    # Use the Apple icon.
+    'https://gist.githubusercontent.com/sorin-ionescu/8118073/raw/e2c62c5a360dc4bccd52c9af36981f84674c8c4f/icon.patch'
+  end
+
+  # Enable the '-S' switch to disable forking for launchd.
+  patch do
+    url 'https://gist.githubusercontent.com/sorin-ionescu/8118073/raw/7734969fe95ea4db4f798fb94084f5d51f1f54bf/launchd.patch'
+    sha1 '66ad6c5bcf9f067fcb041ba2d9c1dbfcf52592d9'
+  end
+
+  # Use FSEvents for filesystem notifications.
+  patch do
+    url 'https://gist.githubusercontent.com/sorin-ionescu/8118073/raw/d7611b4cb3f7e2a47aba61382e31b04cc9ded282/fsevents.patch'
+    sha1 'eb071da25c930982d9bfc285afb494dbb9347378'
   end
 
   def install
     inreplace 'minidlna.conf' do |s|
-      s.gsub! '#db_dir=/var/cache/minidlna', "db_dir=#{var}/db/minidlna"
-      s.gsub! '#log_dir=/var/log', "log_dir=#{var}/log"
+      s.gsub! '#friendly_name=My DLNA Server', "friendly_name=MiniDLNA (#{`hostname -s`.strip})"
+      s.gsub! '#db_dir=/var/cache/minidlna', "db_dir=#{ENV['HOME']}/Library/Application Support/minidlna"
+      s.gsub! '#log_dir=/var/log', "log_dir=#{ENV['HOME']}/Library/Logs"
       s.gsub! '#presentation_url=http://www.mylan/index.php', '#presentation_url=http://localhost:8200/'
+      s.gsub! 'media_dir=/opt', <<-EOS.undent
+        media_dir=P,#{ENV['HOME']}/Pictures
+        media_dir=V,#{ENV['HOME']}/Movies
+        media_dir=A,#{ENV['HOME']}/Music/iTunes/iTunes Music/Music
+        media_dir=V,#{ENV['HOME']}/Music/iTunes/iTunes Music/Movies
+        media_dir=V,#{ENV['HOME']}/Music/iTunes/iTunes Music/TV Shows
+        media_dir=V,#{ENV['HOME']}/Music/iTunes/iTunes Music/Home Videos
+      EOS
     end
 
     # Specify the C langauge version to make it compile.
     ENV.append 'CFLAGS', '-std=gnu89'
 
     args = %W[
-      --disable-debug
       --disable-dependency-tracking
       --disable-silent-rules
       --prefix=#{prefix}
+      --sbindir=#{bin}
     ]
+
     args << "--enable-tivo" if build.with? 'tivo'
 
-    system './autogen.sh'
-    system "./configure", *args
-    system 'make install'
+    system './autogen.sh' if build.head?
+    system './configure', *args
+    system 'make'
 
+    bin.install 'minidlnad'
     etc.install 'minidlna.conf'
     man5.install 'minidlna.conf.5'
     man8.install 'minidlnad.8'
   end
 
   test do
-    system "#{sbin}/minidlnad", "-V"
+    system "#{bin}/minidlnad", "-V"
+  end
+
+  def caveats; <<-EOS.undent
+    This formula pre-configurers MiniDLNA for single user mode.
+
+    Link the configuration file and edit it as needed:
+
+        ln -sfv #{etc}/minidlna.conf ~/Library/Preferences/minidlna.conf
+
+    Start MiniDLNA manually:
+
+        minidlnad -f ~/Library/Preferences/minidlna.conf -P ~/Library/Caches/minidlnad.pid
+    EOS
   end
 
   def plist; <<-EOS.undent
@@ -73,14 +110,16 @@ class Minidlna < Formula
         <true/>
         <key>KeepAlive</key>
         <true/>
+        <key>EnableGlobbing</key>
+        <true/>
         <key>ProgramArguments</key>
         <array>
-            <string>#{opt_prefix}/sbin/minidlnad</string>
+            <string>#{opt_prefix}/bin/minidlnad</string>
             <string>-S</string>
             <string>-f</string>
-            <string>#{etc}/minidlna.conf</string>
+            <string>#{ENV['HOME']}/Library/Preferences/minidlna.conf</string>
             <string>-P</string>
-            <string>#{var}/run/minidlna.pid</string>
+            <string>#{ENV['HOME']}/Library/Caches/minidlnad.pid</string>
         </array>
         <key>WorkingDirectory</key>
         <string>#{HOMEBREW_PREFIX}</string>
@@ -89,4 +128,3 @@ class Minidlna < Formula
     EOS
   end
 end
-
